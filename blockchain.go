3ad0a4f6bdc57445dbebaf96fbce169bd8e09ed6
obynonwane <biostechnologyng@gmail.com>
2024-03-20 09:48:50 +0100

    updated and added blockchain struct creation

--- blockchain.go
+++ blockchain.go
@@ -24,16 +24,31 @@ func NewBlock(nonce int, previousHash string) *Block {
 }
 
 func (b *Block) Print() {
-	fmt.Printf("timestamp       %d\n", b.timestamp)
-	fmt.Printf("nonce       %d\n", b.nonce)
+	fmt.Printf("timestamp           %d\n", b.timestamp)
+	fmt.Printf("nonce               %d\n", b.nonce)
 	fmt.Printf("previous_hash       %s\n", b.previousHash)
-	fmt.Printf("transactions       %s\n", b.transactions)
+	fmt.Printf("transactions        %s\n", b.transactions)
 }
+
+// define the blockchain struct
+type Blockchain struct {
+	transactionPool []string
+	chain           []*Block
+}
+
+func (bc *Blockchain) CreateBlock(nonce int, previousHash string) *Block {
+	b := NewBlock(nonce, previousHash)
+	bc.chain = append(bc.chain, b)
+
+	return b
+}
+
 func init() {
 	log.SetPrefix("Blockchain: ")
 }
 
 func main() {
+	//calling the block instance in the main function
 	b := NewBlock(0, "init hash")
 	b.Print()
 }
